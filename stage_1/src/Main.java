f74e4a075682e27d0b9b1a5421af8a006dd9b116
horia <ignat.horia.andrei@gmail.com>
2020-12-06 20:16:57 +0200

    add skel

---
+++ stage_1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        String input = args[0];
+        String output = args[1];
+        System.out.println(input + " " + output);
+    }
+}
