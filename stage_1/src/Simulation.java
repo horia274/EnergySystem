39f9c33ba0075cddb6702602e4c84af4fed79303
horia <ignat.horia.andrei@gmail.com>
2020-12-08 12:53:12 +0200

    [stage1] Simulation

--- stage_1/src/Simulation.java
+++ stage_1/src/Simulation.java
@@ -41,6 +41,63 @@ public class Simulation {
         return distributors;
     }
 
+    private void firstTurn() {
+        for (Distributor distributor : distributors) {
+            if (!distributor.isBankrupt()) {
+                distributor.computeContractPrice();
+                distributor.removeInvalidContracts();
+            }
+        }
+
+        for (Consumer consumer : consumers) {
+            if (!consumer.isBankrupt()) {
+                consumer.earnSalary();
+                consumer.chooseContract(distributors);
+
+                Contract contract = consumer.getContract();
+                Distributor chosenDistributor = contract.getDistributor();
+                chosenDistributor.addContract(contract);
+
+                consumer.payBill();
+                contract.decreaseContractMonths();
+            }
+        }
+
+        for(Distributor distributor : distributors) {
+            if (!distributor.isBankrupt()) {
+                distributor.removeContractsIfIsBankrupt();
+                distributor.earnMoneyFromConsumers();
+                distributor.payBill();
+                distributor.removeBankruptConsumers();
+            }
+        }
+    }
+
+    private void simulateTurn(MonthlyUpdateInputData monthlyUpdate) {
+        List<ConsumerInputData> newConsumers = monthlyUpdate.getNewConsumers();
+        List<CostChangesInputData> costsChanges = monthlyUpdate.getCostsChanges();
+
+        for (ConsumerInputData newConsumer : newConsumers) {
+            Consumer currentConsumer;
+            currentConsumer = ConsumerFactory.getInstance().createConsumer(ConsumerFactory.ConsumerType.Concrete, newConsumer);
+            consumers.add(currentConsumer);
+        }
+
+        for (CostChangesInputData costChanges : costsChanges) {
+            Distributor currentDistributor = Distributor.findDistributor(distributors, costChanges.getId());
+            if (currentDistributor != null) {
+                currentDistributor.setInfrastructureCost(costChanges.getInfrastructureCost());
+                currentDistributor.setProductionCost(costChanges.getProductionCost());
+            }
+        }
+
+        firstTurn();
+    }
+
     public void simulateAllTurns() {
+        firstTurn();
+        for (MonthlyUpdateInputData monthlyUpdate : monthlyUpdates) {
+            simulateTurn(monthlyUpdate);
+        }
     }
 }
